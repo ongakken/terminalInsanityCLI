65a74906cf3cf840784999a2b7da318244a82ee4
Simon Slamka <51794014+simonSlamka@users.noreply.github.com>
2021-05-03 12:30:44 +0200

    init;

--- terminalInsanityCONSOLE/main.cpp
+++ terminalInsanityCONSOLE/main.cpp
@@ -1,7 +1,25 @@
+﻿// © Copyright 2020 - 2021 Ongakken s.r.o. All rights reserved.
+// Ongakken, the alternative names オンガッケン, オンがッけン, the Ongakken logo, the Ongakken symbol, the Ongakken pattern are trademarks of Ongakken s.r.o.
+
+#include "main.h"
 #include <cstdio>
+#include <iostream>
+
+using namespace std; //standard namespace for using cout, cin and some other without defining the namespace they're in (std::cout)
 
 int main()
 {
-    printf("hello from terminalInsanityCONSOLE!\n");
+	core Core;
+	cout << "hello from terminalInsanityCONSOLE!\n";
+	Core.init();
     return 0;
+}
+
+int core::init()
+{
+	system("[ -d /opt/ongakken/terminalInsanity ] && peep success");
+	system("[ -d /opt/ongakken/terminalInsanity/sounds ] && peep success");
+	system("[ -d /opt/ongakken/terminalInsanity/img ] && peep success");
+	system("'peep successALL'");
+	return 0;
 }
---
+++ terminalInsanityCONSOLE/main.h
@@ -0,0 +1,13 @@
+﻿// © Copyright 2020 - 2021 Ongakken s.r.o. All rights reserved.
+// Ongakken, the alternative names オンガッケン, オンがッけン, the Ongakken logo, the Ongakken symbol, the Ongakken pattern are trademarks of Ongakken s.r.o.
+
+#pragma once
+
+class core
+{
+public:
+	int init();
+};
+
+
+
