43798d2b0540738e56889928251e38e6cdf2187d
Simon Slamka <51794014+simonSlamka@users.noreply.github.com>
2021-05-15 22:35:14 +0200

    it works xD; a lot of stuff done, wrote the func to evaluate commands from stdinput and a func to iterate over a string and print out individual characters with a delay in between them (to simulate actual RT typing);

--- terminalInsanityCONSOLE/main.cpp
+++ terminalInsanityCONSOLE/main.cpp
@@ -1,38 +1,84 @@
 ﻿// © Copyright 2021 Ongakken s.r.o. All rights reserved.
-//   Terminal Insanity, Ongakken, the alternative names オンガッケン, オンがッけン, the Ongakken logo, the Ongakken symbol, the Ongakken pattern are trademarks of Ongakken s.r.o.
-//   Based on a 2016 videogame 'Terminal Insanity' by Simon Slamka
+//   Terminal Insanity, Ongakken, the alternative names オンガッケン,  オンがッけン, the Ongakken logo, the Ongakken symbol, the Ongakken pattern are trademarks of Ongakken s.r.o.
+// Based on a 2016 videogame 'Terminal Insanity' by Simon Slamka
 
 #include "main.h"
 
-//using namespace std; //standard namespace for using cout, cin and some other without defining the namespace they're in (std::cout)
+// using namespace std; //standard namespace for using cout, cin and some other without defining the namespace they're in (std::cout)
 
-int main()
+uint64_t constexpr mix(char m, uint64_t s)
 {
+	return ((s << 7) + ~(s >> 3)) + ~m;
+}
+
+uint64_t constexpr hashIt(const char* m)
+{
+	return (*m) ? mix(*m, hashIt(m + 1)) : 0;
+}
+
+int main() {
 	core Core;
 	cout << "Terminal Insanity 0.00.03\n";
 	Core.init();
 	Core.boot();
 	Core.lvl1();
-    return 0;
+	string testInput;
+	bool bInteractiveShell = true;
+	//cout << "$  ";
+	//cin >> testInput;
+	//const char* testInputChar = testInput.c_str();
+	//Core.evaluateCmdInput(testInputChar);
+	while(bInteractiveShell)
+	{
+		cout << "$ ";
+		cin >> testInput;
+		const char* testInputChar = testInput.c_str();
+		Core.evaluateCmdInput(testInputChar);
+
+	}
+	return 0;
 }
 
-void core::iterateOverString()
-{
-
+void core::iterateOverString(string &playerMsg, int s) {
+	for(int i = 0; i < playerMsg.size(); i++)
+	{
+		cout << playerMsg[i] << flush;
+		usleep(s);
+	}
 }
 
-int core::init()
+void core::evaluateCmdInput(const char* cmdInputChar)
 {
-	//string x;
+	switch(hashIt(cmdInputChar))
+	{
+		case hashIt("whoami"):
+			whoami();
+			break;
+		default:
+			cout << "ongashell: command not found: " << cmdInputChar << endl;
+			break;
+	};
+}
+
+int core::whoami()
+{
+	cout << "root\n";
+	return 0;
+}
+
+
+int core::init() {
+	// string x;
 	system("/usr/bin/zsh -c '[ -d /opt/ongakken/terminalInsanity ] && [ -d /opt/ongakken/terminalInsanity/sounds ] && [ -d /opt/ongakken/terminalInsanity/img ] && printf '\a''");
 	sleep(1);
 	system("grep 'zsh' /etc/shells >> /dev/null && printf '\a'");
 	sleep(1);
 	system("./zshVerify >> /dev/null && printf '\a'");
-	//cout << "$ ";
-	//getline(cin, x, '\n');
-	//system("! /bin/sed -r -e 's/\x0.*//' /proc/$$/cmdline | grep 'zsh' >> /dev/null && /usr/bin/zsh");
-	//system("/bin/sed -r -e 's/\x0.*//' /proc/$$/cmdline | grep 'zsh'");
+	// cout << "$ ";
+	// getline(cin, x, '\n');
+	// system("! /bin/sed -r -e 's/\x0.*//' /proc/$$/cmdline | grep 'zsh' >>
+	// /dev/null && /usr/bin/zsh"); system("/bin/sed -r -e 's/\x0.*//'
+	// /proc/$$/cmdline | grep 'zsh'");
 	usleep(500000);
 	system("kitty -o allow_remote_control=yes --title OngakkenLogo --listen-on unix:/tmp/terminalInsanity --hold viu /opt/ongakken/logo.png &");
 	system("paplay /opt/ongakken/terminalInsanity/sounds/intro.wav &");
@@ -43,9 +89,10 @@ int core::init()
 	return 0;
 }
 
-int core::boot()
-{
-	cout << "\033]0;" << "Terminal" << "\007";
+int core::boot() {
+	cout << "\033]0;"
+		<< "Terminal"
+		<< "\007";
 	system("clear");
 	cout << "Booting ...\n\n";
 	sleep(2);
@@ -59,21 +106,22 @@ int core::boot()
 	cout << "\n";
 	sleep(1);
 	ifstream f("/opt/ongakken/terminalInsanity/dmesg");
-	if (f.is_open())
-	{
-		cout << f.rdbuf();
+	if (f.is_open()) {
+	cout << f.rdbuf();
 	}
 	cout << "\n";
-	system("printf '[\e[31mFAIL\e[0m] Scanning for friendly network devices ...\n'");
+	system(
+		"printf '[\e[31mFAIL\e[0m] Scanning for friendly network devices ...\n'");
 	cout << "\n";
 	sleep(4);
-	system("printf '[\e[32mOK\e[0m] Running post-boot runlevel subroutine 'readme' ...'");
+	system("printf '[\e[32mOK\e[0m] Running post-boot runlevel subroutine "
+			"'readme' ...'");
 	sleep(1);
 	system("clear");
+	return 0;
 }
 
-int core::lvl1()
-{
+int core::lvl1() {
 	cout << "\033]0;" << "Terminal" << "\007";
 	system("clear");
 	sleep(3);
@@ -94,8 +142,10 @@ int core::lvl1()
 	system("printf '\e[95mMark:\e[0m idk\n'");
 	cout << "\n";
 	sleep(4);
-	system("printf '%s' '\e[36mJ3ff:\e[0m' && printf ' Hey guys\n' | pv -qL 9");
-	cout << "\n";
+	system("printf '%s' '\e[36mJ3ff:\e[0m'");
+	playerMsg = (" Hey guys");
+	iterateOverString(playerMsg, 100000);
+	cout << "\n\n";
 	sleep(6);
 	system("printf '\e[95mMark:\e[0m nice of you to finally show up..\n'");
 	cout << "\n";
@@ -105,7 +155,29 @@ int core::lvl1()
 	system("printf '\e[34mJason:\e[0m we were just discussing the plan\n'");
 	cout << "\n";
 	sleep(3);
-	system("printf '%s' '\e[36mJ3ff:\e[0m' && printf ' So how are we gonna do it?\n' | pv -qL 9");
-
+	system("printf '%s' '\e[36mJ3ff:\e[0m'");
+	playerMsg = (" So how are we gonna do it?");
+	iterateOverString(playerMsg, 100000);
+	cout << "\n\n";
 	sleep(9);
+	system("printf '\e[95mMark:\e[0m well, for starters, we need more people.\n'");
+	sleep(7);
+	system("printf '\e[95mMark:\e[0m even though we can do it skill-wise, we do not have time, so we need more firepower\n'");
+	sleep(11);
+	system("printf '\e[95mMark:\e[0m i have an idea, but it requires putting aurora up to a difficult recursive search task\n'");
+	sleep(12);
+	system("printf '\e[95mMark:\e[0m so j3ff, do i have your permission to access her and run the code i wrote?\n'");
+	cout << "\n";
+	sleep(4);
+	system("printf '%s' '\e[36mJ3ff:\e[0m'");
+	playerMsg = (" Alright, try it. We need this to happen.");
+	iterateOverString(playerMsg, 100000);
+	cout << "\n\n";
+	sleep(5);
+	system("printf '\e[95mMark:\e[0m roger. on it\n'");
+	cout << "\n\n";
+	sleep(4);
+	system("printf '%s' '\e[33mA week later ...\e[0m\n' | pv -qL 3");
+	cout << "\n\n";
+	sleep(2);
 }
--- terminalInsanityCONSOLE/main.h
+++ terminalInsanityCONSOLE/main.h
@@ -1,26 +1,42 @@
 ﻿// © Copyright 2021 Ongakken s.r.o. All rights reserved.
 //   Ongakken, the alternative names オンガッケン, オンがッけン, the Ongakken logo, the Ongakken symbol, the Ongakken pattern are trademarks of Ongakken s.r.o.
 
-#pragma once
+#pragma once //include this file (and any files that reference this header file) exactly once in the compilation
 
 using namespace std; //standard namespace for using cout, cin and some other without defining the namespace they're in (std::cout)
 
-#ifdef _WIN32
+#ifdef _WIN32 //is the current platform Windows? load the system lib
 #include <Windows.h>
-#else
+#else //are we on anything else? load the POSIX unix sys lib
 #include <unistd.h>
 #endif
-#include <cstdio>
-#include <iostream>
-#include <fstream>
+//#include <cstdio> //we don't really need this lib right now, but keeping it here just in case. I mean, there's gotta be a reason why I put it in in the first place, right? xD
+#include <iostream> //standard C++ I/O stream lib, needed for stuff like cout and cin
+#include <fstream> //standard C++ file stream lib, needed for loading, reading writing to or otherwise manipulating files
+#include <string>
+
 
 class core
 {
 public:
-	int init();
-	int boot();
-	int lvl1();
-	void iterateOverString();
+	////////// core vars and methods that run the game itself //////////
+
+	int init(); //this method checks the runtime dependencies and verifies if all dirs are existing; if not, it creates them. we also play the intro seq here
+	int boot(); //this method runs the simulated boot seq of the in-game computer
+	int lvl1(); //level methods -- these run code relevant for each level of the game
+	void iterateOverString(string &playerMsg, int s); //a method to iterate through a "msg" string and print out characters divided by an 's' amount of sleep (in seconds)
+	void evaluateCmdInput(const char* cmdInputChar);
+	string playerMsg; //a string to be iterated over using the iterateOverString method
+	string cmdInput; //command input which the player will enter while having access to an interactive shell - this value will be processed and evaluated to see if the command entered exists
+	//list<string> commandsList = { "whoami", "help" };
+
+	////////////////////////////////////////////////////////////////////
+
+	////////// command methods - methods that will execute stuff based on what shell command they represent //////////
+
+	int whoami();
+
+	//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 };
 
 
